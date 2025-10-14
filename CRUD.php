3b6af613b0b963cc9ab61dc5ea89b4caaee68968
joao25104708360011-dot <joao.25104708360011@faeterj-rio.edu.br>
2025-10-14 09:52:30 -0300

    Add files via upload

---
+++ CRUD.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $host = '127.0.0.1';
+        $db   = 'CRUD';
+        $user = 'seu_usuario';
+        $pass = 'sua_senha';
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FECTH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    ?>
+</body>
+</html>
